ec7e4bc1ddc95f9089e137136c95cfd7484758fc
leeotus <leeotus@163.com>
2025-09-19 17:53:13 +0800

    from OS boot to loader

--- script/qemu-debug-linux.sh
+++ script/qemu-debug-linux.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 # 适用于Linux
-export DISPLAY=localhost:10.0
+export DISPLAY=localhost:10.0   # 这里我自己是远程连接自己的Ubuntu,通过mobaxterm来转发
 qemu-system-i386 -daemonize -m 128M -s -S -drive file=disk1.img,index=0,media=disk,format=raw -drive file=disk2.img,index=1,media=disk,format=raw -d pcall,page,mmu,cpu_reset,guest_errors,page,trace:ps2_keyboard_set_translation -display sdl,gl=on -vga std -monitor tcp:127.0.0.1:4444,server,nowait
 # qemu-system-i386 -daemonize -m 128M -s -S -drive file=disk1.img,index=0,media=disk,format=raw -drive file=disk2.img,index=1,media=disk,format=raw -d pcall,page,mmu,cpu_reset,guest_errors,page,trace:ps2_keyboard_set_translation
---
+++ source/loader/loader_16.c
@@ -0,0 +1,15 @@
+__asm__(".code16gcc");
+
+/**
+ * @file loader_16.c
+ * @author leeotus (leeotus@163.com)
+ * @brief 16位实模式下的代码
+ */
+
+#include "loader.h"
+
+void loader_entry(void) {
+    for(;;) {
+
+    }
+}
